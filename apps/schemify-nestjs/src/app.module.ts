0ae0457af8147c60eadf7b8256ffe2d102faf97b
ixyzdev <ixyz.dev@gmail.com>
2025-04-14 00:21:35 -0400

    fix(kafka)

--- apps/schemify-nestjs/src/app.module.ts
+++ apps/schemify-nestjs/src/app.module.ts
@@ -1,27 +1,9 @@
 import { Module } from '@nestjs/common'
 import { ExampleModule } from './example/example.module'
-import { ClientsModule, Transport } from '@nestjs/microservices'
 
 @Module({
-  imports: [
-    ClientsModule.register([
-      {
-        name: 'EXAMPLE_KAFKA_CLIENT',
-        transport: Transport.KAFKA,
-        options: {
-          client: {
-            brokers: ['kafka1:9092', 'kafka2:9092'],
-            clientId: 'example-schemify-nestjs'
-          },
-          producer: {
-            allowAutoTopicCreation: true
-          },
-          producerOnlyMode: true
-        }
-      }
-    ]),
-    ExampleModule
-  ],
+  imports: [ExampleModule],
+
   controllers: [],
   providers: []
 })
--- apps/schemify-nestjs/src/example/example.module.ts
+++ apps/schemify-nestjs/src/example/example.module.ts
@@ -1,8 +1,27 @@
 import { Module } from '@nestjs/common'
 import { ExampleService } from './example.service'
 import { ExampleController } from './example.controller'
+import { ClientsModule, Transport } from '@nestjs/microservices'
 
 @Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'EXAMPLE_KAFKA_CLIENT',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            brokers: ['kafka1:9092', 'kafka2:9092'],
+            clientId: 'example-schemify-nestjs'
+          },
+          producer: {
+            allowAutoTopicCreation: true
+          },
+          producerOnlyMode: true
+        }
+      }
+    ])
+  ],
   controllers: [ExampleController],
   providers: [ExampleService]
 })
