a4834e13c081ce0e24e6c1b2e08fea53da5940ca
ixyzdev <ixyz.dev@gmail.com>
2025-04-14 19:57:51 -0400

    B

--- apps/schemify-nestjs/src/example/example.controller.ts
+++ apps/schemify-nestjs/src/example/example.controller.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-base-to-string */
 /* eslint-disable @typescript-eslint/restrict-template-expressions */
 /* eslint-disable @darraghor/nestjs-typed/controllers-should-supply-api-tags */
